8d23e4bd26cc2b256860339fff86c9dcf31f266b
dlawr <dan.lawrence3@gmail.com>
2016-08-26 13:47:02 -0400

    created footer

---
+++ partials/footer.js
@@ -0,0 +1,4 @@
+document.write('<ul>');
+document.write(  '<li><a href="aboutme.html">About Me</a></li>');
+document.write(  '<li><a href="contact.html">Contact</a></li>');
+document.write('</ul>');
