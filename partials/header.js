7c9fefd2c7c34c2c9bf06966e3f1e3d908787639
dlawr <dan.lawrence3@gmail.com>
2016-08-26 13:41:29 -0400

    header partial loading properly in about

--- partials/header.js
+++ partials/header.js
@@ -1,8 +1,8 @@
-document.write(<header>);
-document.write(  <img src="./assets/portrait.jpg">);
-document.write(  <h1>Web Developer</h1>);
-document.write(  <ul>);
-document.write(    <li><a href="aboutme.html">About Me</a></li>);
-document.write(    <li><a href="contact.html">Contact</a></li>);
-document.write(  </ul>);
-document.write(</header>);
+document.write('<header>');
+document.write(  '<img src="./assets/portrait.jpg">');
+document.write(  '<h1>Web Developer</h1>');
+document.write(  '<ul>');
+document.write(    '<li><a href="aboutme.html">About Me</a></li>');
+document.write(    '<li><a href="contact.html">Contact</a></li>');
+document.write(  '</ul>');
+document.write('</header>');
