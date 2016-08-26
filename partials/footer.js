b714780531f6c4e15afd540ed81d915ae082280b
dlawr <dan.lawrence3@gmail.com>
2016-08-26 14:06:37 -0400

    fixed links

--- partials/footer.js
+++ partials/footer.js
@@ -1,7 +1,7 @@
 document.write('<footer>');
 document.write(  '<ul>');
 document.write(    '<li><a href="dan.lawrence3@gmail.com">email</a></li>');
-document.write(    '<li><a href="linkedin.com/dlawr">LinkedIn</a></li>');
-document.write(    '<li><a href="github.com/dlawr">GitHub</a></li>');
+document.write(    '<li><a href="https://www.linkedin.com/in/dlawr">LinkedIn</a></li>');
+document.write(    '<li><a href="https://www.github.com/dlawr">GitHub</a></li>');
 document.write(  '</ul>');
 document.write('</footer>');
