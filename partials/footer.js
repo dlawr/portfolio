8aa31941fee774a9b11168ecd96c7315734693d9
dlawr <dan.lawrence3@gmail.com>
2016-08-26 14:16:33 -0400

    fixed mailto link in footer

--- partials/footer.js
+++ partials/footer.js
@@ -1,6 +1,6 @@
 document.write('<footer>');
 document.write(  '<ul>');
-document.write(    '<li><a href="dan.lawrence3@gmail.com">email</a></li>');
+document.write(    '<li><a href="mailto:dan.lawrence3@gmail.com">email</a></li>');
 document.write(    '<li><a href="https://www.linkedin.com/in/dlawr">LinkedIn</a></li>');
 document.write(    '<li><a href="https://www.github.com/dlawr">GitHub</a></li>');
 document.write(  '</ul>');
