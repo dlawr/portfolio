01602efda5ab241b7acfe1cbddd4a832269417da
dlawr <dan.lawrence3@gmail.com>
2016-08-26 16:19:56 -0400

    added portfolio to nav

--- partials/header.js
+++ partials/header.js
@@ -3,6 +3,7 @@ document.write(  '<img src="./assets/portrait.jpg">');
 document.write(  '<h1>Web Developer</h1>');
 document.write(  '<ul>');
 document.write(    '<li><a href="aboutme.html">About Me</a></li>');
+document.write(    '<li><a href="portfolio.html">Portfolio</a></li>');
 document.write(    '<li><a href="contact.html">Contact</a></li>');
 document.write(  '</ul>');
 document.write('</header>');
