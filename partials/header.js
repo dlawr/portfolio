40f764f0fe13a07d2fd31ecfa9a32de53012da3d
dlawr <dan.lawrence3@gmail.com>
2016-08-26 13:28:42 -0400

    use js to write html

--- partials/header.js
+++ partials/header.js
@@ -1,9 +1,8 @@
-<!DOCTYPE html>
-<header>
-  <img src="./assets/portrait.jpg">
-  <h1>Web Developer</h1>
-  <ul>
-    <li><a href="aboutme.html">About Me</a></li>
-    <li><a href="contact.html">Contact</a></li>
-  </ul>
-</header>
+document.write(<header>);
+document.write(  <img src="./assets/portrait.jpg">);
+document.write(  <h1>Web Developer</h1>);
+document.write(  <ul>);
+document.write(    <li><a href="aboutme.html">About Me</a></li>);
+document.write(    <li><a href="contact.html">Contact</a></li>);
+document.write(  </ul>);
+document.write(</header>);
